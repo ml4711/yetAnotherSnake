43bdbd48f83863bd0d4bd086d0b4f064f89fee10
ml4711 <markus.lanser@gmail.com>
2020-10-13 14:57:07 +0100

    add controls

--- main.py
+++ main.py
@@ -1,14 +1,55 @@
 import pygame
 
 class cube(object):
-	pass
+	rows = 0
+	w = 0
+	def __init__(self, start, dirnx=1, dirny=0, color=(255,0,0)):
+		pass
+
+	def move(self, dirnx, dirny):
+		pass
+
+	def draw(self, surface, eyes=False):
+		pass
 
 class snake(object):
+	body = []
+	turn = {}
+
 	def __init__(self, color, pos):
-		pass
+		self.color = color
+		self.head = cube(pos)
+		self.body.append(self.head)
+		self.dirnx = 0
+		self.dirny = 1
 
 	def move(self):
-		pass
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+
+			keys = pygame.key.get_pressed()
+
+			for key in keys:
+				if keys[pygame.K_LEFT]:
+					self.dirnx = -1
+					self.dirny = 0
+					self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+				elif keys[pygame.K_RIGHT]:
+					self.dirnx = 1
+					self.dirny = 0
+					self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+				elif keys[pygame.K_UP]:
+					self.dirnx = 0
+					self.dirny = -1
+					self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+				elif keys[pygame.K_DOWN]:
+					self.dirnx = 0
+					self.dirny = 1
+					self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
 
 	def reset(self, pos):
 		pass
